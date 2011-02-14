4fb1c0a9d7afdc7e8d6429a20db10f89ccd19136
Michael Schmitz <lydianblues@gmail.com>
2011-02-14 15:30:47 -0800

    product form builder checkpoint

--- app/form_builders/product_form_builder.rb
+++ app/form_builders/product_form_builder.rb
@@ -1,19 +1,40 @@
 class ProductFormBuilder < ActionView::Helpers::FormBuilder
 
-  def text_field(method, options = { })
+  # Generate the following html:
+  #  <div class='input-field'>
+  #    <label for='name'>
+  #      label text from options[:label]
+  #    </label>
+  #    <span class='field-error'>
+  #      Error message
+  #    </span>
+  #    <input size='40' type='text' />
+  # </div>
+  #
+  def text_field(method, options = {})
+    label_text = options.delete(:label)
+    label_text += "*" if attr_required?(method)
     @template.content_tag(:div, :class => "input-field") do
-      label(:for => "name") +
+      label(method, label_text) +
       @template.content_tag(:span, "Error Message", :class => "field-error") +
-      @template.content_tag(:br) +
-      @template.content_tag(:input, :type => "text", :size => options[:size]) 
-      
-    end      
+      super
+    end.html_safe
   end
+
+  def text_area(method, options = {})
+    label_text = options.delete(:label)
+    label_text += "*" if attr_required?(method)
+    @template.content_tag(:div, :class => "input-field") do
+      label(method, label_text) +
+      @template.content_tag(:span, "Error Message", :class => "field-error") +
+      super
+    end.html_safe
+  end      
 
   private
 
-  # This is also in application_helpers.rb, and in labeled_form_builder.rb.
-  def attr_required?(klass, attribute)
+  def attr_required?(attribute)
+    klass = Kernel.const_get((object_name.to_s).camelize)
     if klass.respond_to?(:validators_on)
       klass.validators_on(attribute).map(&:class).include?(
         ActiveModel::Validations::PresenceValidator)
@@ -22,12 +43,5 @@ class ProductFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
-  def field_label(method, options)
-    klass = Kernel.const_get((self.object_name.to_s).camelize)
-    if attr_required?(klass, method)
-      options[:label] += '*'
-    end
-    label(method, options[:label], :class => "form-elt-left")
-  end
   
 end
